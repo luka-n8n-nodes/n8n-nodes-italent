2a8f4c2d7f421364cd846ebf39d0480e3dda71e3
luka-mimi <luka.cat.mimi@gmail.com>
2025-12-11 14:25:09 +0800

    fix: 修复无法输出到错误分支的问题

--- nodes/ITalent/ITalent.node.ts
+++ nodes/ITalent/ITalent.node.ts
@@ -4,9 +4,12 @@ import {
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
+	NodeConnectionTypes,
 	NodeOperationError,
 } from 'n8n-workflow';
 import ResourceFactory from '../help/builder/ResourceFactory';
+import { configuredOutputs } from '../help/utils/parameters';
+import { Credentials, OutputType } from '../help/type/enums';
 
 const resourceBuilder = ResourceFactory.build(__dirname);
 
@@ -23,13 +26,11 @@ export class ITalent implements INodeType {
 			name: '北森iTalent',
 		},
 		usableAsTool: true,
-		// @ts-ignore
-		inputs: ['main'],
-		// @ts-ignore
-		outputs: ['main'],
+		inputs: [NodeConnectionTypes.Main],
+		outputs: `={{(${configuredOutputs})($parameter)}}`,
 		credentials: [
 			{
-				name: 'iTalentApi',
+				name: Credentials.ITalentApi,
 				required: true,
 			},
 		],
@@ -39,8 +40,8 @@ export class ITalent implements INodeType {
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
 
-		let responseData: IDataObject = {};
-		let returnData: INodeExecutionData[] = [];
+		// 使用数组初始化，支持多输出
+		let returnData: INodeExecutionData[][] = Array.from({ length: 1 }, () => []);
 
 		const resource = this.getNodeParameter('resource', 0);
 		const operation = this.getNodeParameter('operation', 0);
@@ -62,7 +63,29 @@ export class ITalent implements INodeType {
 					itemIndex,
 				});
 
-				responseData = await callFunc.call(this, itemIndex);
+				const responseData = await callFunc.call(this, itemIndex);
+
+				// 检查是否有自定义输出类型
+				if (responseData && typeof responseData === 'object' && 'outputType' in responseData) {
+					const typedResponse = responseData as { outputType: OutputType; outputData?: INodeExecutionData[][] };
+					const { outputType } = typedResponse;
+
+					if (outputType === OutputType.Multiple && typedResponse.outputData) {
+						// 多输出模式：直接使用返回的输出数据
+						returnData = typedResponse.outputData;
+					} else if (outputType === OutputType.None) {
+						// 无输出模式
+						return [];
+					}
+					// OutputType.Single 会走下面的默认处理
+				} else {
+					// 默认单输出模式
+					const executionData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray(responseData as IDataObject),
+						{ itemData: { item: itemIndex } },
+					);
+					returnData[0].push(...executionData);
+				}
 			} catch (error) {
 				this.logger.error('call function error', {
 					resource,
@@ -73,17 +96,17 @@ export class ITalent implements INodeType {
 				});
 
 				if (this.continueOnFail()) {
-					let errorJson = {
-						error: error.message,
-					};
-					if (error.name === 'NodeApiError') {
-						errorJson.error = error?.cause?.error;
-					}
-
-					returnData.push({
-						json: errorJson,
-						pairedItem: itemIndex,
-					});
+					// 优化错误信息提取，优先使用 description
+					const executionErrorData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray({
+							error: error.description ?? error.message,
+							...(error.name === 'NodeApiError' && error.cause?.error
+								? { details: error.cause.error }
+								: {}),
+						}),
+						{ itemData: { item: itemIndex } },
+					);
+					returnData[0].push(...executionErrorData);
 					continue;
 				} else if (error.name === 'NodeApiError') {
 					throw error;
@@ -94,14 +117,9 @@ export class ITalent implements INodeType {
 					});
 				}
 			}
-			const executionData = this.helpers.constructExecutionMetaData(
-				this.helpers.returnJsonArray(responseData as IDataObject),
-				{ itemData: { item: itemIndex } },
-			);
-			returnData.push(...executionData);
 		}
 
-		return [returnData];
+		return returnData;
 	}
 }
 
